01f01d7688829cec56be3f4b03121019b841572f
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-10-21 17:25:29 +0100

    add main state machine, begin updating typedefs and style

---
+++ src/protocol.h
@@ -0,0 +1,27 @@
+#ifndef __PTPD_PROTOCOL_H__
+#define __PTPD_PROTOCOL_H__
+
+/**
+ * @file
+ * @brief ptpd-lwip protocol interface
+ *
+ * @author @htmlonly &copy; @endhtmlonly 2020 James Bennion-Pedley
+ *
+ * @date 1 Oct 2020
+ */
+
+#include "def/datatypes_private.h"
+
+/**
+ * \brief Run PTP stack in current state
+ * Handle actions and events for 'port_state'
+ */
+void doState(ptpClock_t *ptpClock);
+
+/**
+ * \brief Change state of PTP stack
+ * Perform actions required when leaving 'port_state' and entering 'state'
+ */
+void toState(ptpClock_t *ptpClock, u8_t state);
+
+#endif /* __PTPD_PROTOCOL_H__ */
